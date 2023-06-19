4d36a57aa76a30569ffc851cd085ba4d2cd8ddcb
Ronny García Zambrano <rsgarcia@espol.edu.ec>
2023-06-19 10:31:18 -0500

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
-
+(function () {
+  let URL =
+    "https://api.open-meteo.com/v1/forecast?latitude=-2.15&longitude=-79.97&hourly=temperature_2m&daily=uv_index_max&timezone=auto";
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("lat").innerText(data.latitude);
+      document.getElementById("long").innerText(data.longitude);
+    })
+    .catch((error) => console.log(error));
+});
